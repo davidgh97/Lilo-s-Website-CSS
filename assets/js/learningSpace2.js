9acc6621227dd51cde5acb9a3c7ce05b365ebe6c
davidgh97 <124364748+davidgh97@users.noreply.github.com>
2023-02-16 16:41:48 +0200

    Add files via upload
    Adding files

---
+++ assets/js/learningSpace2.js
@@ -0,0 +1,38 @@
+// // const htmlBody = document.querySelector('body');
+
+// // const clickFunction = function () {
+// //     const colors = ["#002942", "#0Ca7DB", "red", "blue", "yellow", "green", "orange", "purple"];
+// //     const randomNumber = Math.random();
+// //     console.log("colors.length: " + colors.length);
+// //     console.log("Math.random(): " + randomNumber);
+// //     console.log("Math.random(): " + randomNumber);
+// //     console.log("Math.random() * colors.length: " + randomNumber * colors.length);
+// //     const randomIndex = Math.floor(randomNumber * colors.length);
+// //     console.log("Math.floor: " + randomIndex);
+
+// //     const randomColor = colors[randomIndex];
+// //     htmlBody.style.backgroundColor = randomColor;
+
+// //     console.log(" The user clicked and set the color to: " + randomColor);
+
+// //     console.log(Math.random() * colors.length)
+// //     return "hello";
+// // }
+
+// // htmlBody.onclick = clickFunction;
+
+// // console.log(clickFunction);
+
+// let body = document.querySelector('.body-section'),
+//     babies = body.children;
+
+// console.log(body);
+// console.log(babies);
+
+// // babies.forEach((e) => {
+// //     console.log(e);
+// // });
+
+// for (var i = 0; i < babies.length; i++) {
+//     console.log(babies[i]); //second console output
+// }
